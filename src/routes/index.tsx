4861fedbc90cf3e07501319220dd04902a539dba
christiancesar <christiancnp@gmail.com>
2021-05-05 18:05:05 -0400

    not reload page afer build

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Switch, Route, BrowserRouter } from 'react-router-dom';
-
+import { Route, Switch } from 'react-router-dom';
 import Dashboard from '../pages/Dashboard';
 import Repository from '../pages/Repository';
 
